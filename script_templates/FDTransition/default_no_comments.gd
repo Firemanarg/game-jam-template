84e42aa27febe95ce988ab5acb6b47c225e3391b
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2026-07-17 23:07:21 -0300

    feat: Import StateMachine system

---
+++ script_templates/FDTransition/default_no_comments.gd
@@ -0,0 +1,26 @@
+# meta-name: Default (no comments)
+# meta-description: Initial script containing overridable methods needed to create custom transitions.
+# meta-default: true
+# meta-space-indent: 4
+@tool
+extends FDTransition
+
+
+func _ready() -> void:
+	pass
+
+
+func _setup_play_in() -> void:
+	pass
+
+
+func _setup_play_out() -> void:
+	pass
+
+
+func _on_play_in(ratio: float) -> void:
+	pass
+
+
+func _on_play_out(ratio: float) -> void:
+	pass
